e37a899820ba7af72012bf5d951f65ccce8a7b64
Saonela <777skumbre@gmail.com>
2016-12-14 21:39:01 +0200

    core app

--- main.js
+++ main.js
@@ -1,48 +1,256 @@
 'use strict'
 var request = require('request');
 
-console.log('hey!', process.argv[2]);
 let room = process.argv[2];
 let player = process.argv[3];
 let url = 'http://tictactoe.homedir.eu/game/' + room + '/player/' + player;
-console.log(url)
+let playerSymbol =  process.argv[3] == 1 ? 'x' : 'o';
 
-let message = '[[\"x\", 2, \"y\",  0, \"v\", \"o\"], [\"x\",  0,   \"y\",   0, \"v\", \"x\"], [\"x\", 1,  \"y\",  1,   \"v\", \"x\"],  [\"x\",   2,  \"y\",  2, \"v\", \"o\"]]';
-console.log(message);
-console.log('              ');
+// let message = '[[\"x\", 2, \"y\",  0, \"v\", \"x\"], [\"x\",  0,   \"y\",   0, \"v\", \"x\"], [\"x\", 1,  \"y\",  1,   \"v\", \"x\"],]';
+// let message = '[[\"x\", 2, \"y\",  0, \"v\", \"x\"]]';
 
-parse(message);
-play(url, 'no moves')
+// console.log('PARSE', parse(message));
+
+play(url, '', playerSymbol)
 
 
-function play(url, moves) {
-  console.log(moves);
-  postRequest(url);
-  getRequest(url);
+function play(url, moves, player) {
+  if(moves == '' && player == 'x') {
+    let body = makeMove(moves, player);
+    postRequest(url, body);
+    play(url, body, playerSymbol)
+  } else {
+    getRequest(url, player);
+  }
 }
 
-function postRequest(url) {
+function makeMove (moves, player) {
+  if(moves == '') {
+    return '[[\"x\", 1, \"y\",   1, \"v\", \"' + player + '\"]]';
+  } else {
+    let movesArr = parse(moves);
+    let status = defend(movesArr, player);
+    if(status) {
+      return moves.slice(0, -1) + ', ' + status + ']';
+    } else {
+      return moves.slice(0, -1) + ', ' + makeRandomDiagMove(movesArr, 0, 0, player) + ']';
+    }
+  }
+}
+
+function makeRandomMove(moves, x, y, player) {
+  console.log('makeRandomMove',x,' ',y);
+  let result;
+  if (x == 3) {
+    console.log('no moves left!');
+  } else if (y == 3) {
+    makeRandomMove(moves, x+1, 0, player)
+  } else {
+    let cell = moves.filter((move) => {
+      return move.x == x && move.y == y;
+    });
+    console.log('cell',cell);
+    if(cell.length == 0) {
+      console.log('Kai cia ateina x nuluzta kazkodel.',cell);
+      result =  buildMove(x, y, player);
+      return result;
+    } else {
+      return makeRandomMove(moves, x, y+1 ,player);
+    }
+  }
+  return result;
+}
+
+function makeRandomDiagMove(moves, x, y, player) {
+    console.log('makeRandomDiAGMove',x,' ',y);
+  if (x == 4) {
+    let some =  makeRandomMove(moves, 0, 0, player);
+    console.log('MAKE RANDOM MOVIE RESULT', some)
+    return some;
+  } else if (y == 4) {
+    let some =  makeRandomMove(moves, 0, 0, player);
+    console.log('MAKE RANDOM MOVIE GOOD RESULT', some)
+    return some;
+  } else {
+    if (isCenterFree(moves) && player == 'o') {
+      return makeRandomMove(moves, 0, 0, player);
+    } else {
+      let cell = moves.filter((move) => {
+        return move.x == x && move.y == y;
+      });
+      if(cell.length == 0) {
+        return buildMove(x, y, player);
+      } else {
+        return makeRandomDiagMove(moves, x, y+2 ,player);
+      }
+    }
+  }
+}
+
+function isCenterFree(moves) {
+  return moves.filter((move) => {
+    return move.x == 1 && move.y == 1;
+  });
+}
+
+function defend (moves, player) {
+  let columnStatus = checkColumns(moves,2,player);
+  let rowStatus = checkRows(moves,2,player);
+  let diagonalStatus = checkDiagonals(moves,2,player);
+  if(rowStatus != false) {
+    return '[\"x\",' + rowStatus.x + ', \"y\", ' + rowStatus.y + ', \"v\", \"' + player + '\"]';
+  } else if(columnStatus != false) {
+    return '[\"x\",' + columnStatus.x + ', \"y\", ' + columnStatus.y + ', \"v\", \"' + player + '\"]';
+  } else if(diagonalStatus != false) {
+    return diagonalStatus;
+  } else {
+    return false;
+  }
+}
+
+function checkRows (moves, pos, player) {
+  if(pos == -1) {
+    return false;
+  } else {
+    let row = moves.filter((move) => {
+        return move.x == pos;
+    })
+    let countX = countPlayers(row, 'x')
+    let countO = countPlayers(row, 'o')
+    if((countX == 2 && countO == 0) || (countO == 2  && countX == 0)) {
+        let moveCoor = checkEmpty(row, 'y');
+      if(moveCoor) {
+        return {x:pos, y:moveCoor};
+      }
+    }
+    return checkRows(moves, pos-1, player)
+  }
+}
+
+function checkColumns (moves, pos, player) {
+  if(pos == -1) {
+    return false;
+  } else {
+    let column = moves.filter((move) => {
+        return move.y == pos;
+    })
+    let countX = countPlayers(column, 'x')
+    let countO = countPlayers(column, 'o')
+    if((countX == 2 && countO == 0) || (countO == 2  && countX == 0)) {
+      let moveCoor = checkEmpty(column, 'x');
+      if(moveCoor) {
+        return {x:moveCoor, y:pos};
+      }
+    }
+    return checkColumns(moves, pos-1, player)
+  }
+}
+
+function checkDiagonals (moves, pos, player) {
+  let dg1 = checkDiagonal1(moves, pos, player);
+  let dg2 = checkDiagonal2(moves, pos, player);
+  if(dg1 != false) {
+    return '[\"x\",' + dg1.x + ', \"y\", ' + dg1.y + ', \"v\", \"' + player + '\"]';
+  } else if(dg2 != false) {
+    return '[\"x\",' + dg2.x + ', \"y\", ' + dg2.y + ', \"v\", \"' + player + '\"]';
+  } else {
+    return false;
+  }
+}
+
+function checkDiagonal1 (moves, pos, player) {
+  let diag = moves.filter((move) => {
+      return (move.x == move.y &&  move.player != player);
+  })
+  let countX = countPlayers(diag, 'x')
+  let countO = countPlayers(diag, 'o')
+  if((countX == 2 && countO == 0) || (countO == 2  && countX == 0)) {
+      let moveCoor = checkEmpty(diag, 'x');
+      if(moveCoor) {
+        return {x:moveCoor, y:pos};
+      }
+  }
+  return false;
+}
+
+function checkDiagonal2 (moves, pos, player) {
+  let diag = getDiagonal2(moves,pos,[],player);
+  let countX = countPlayers(diag, 'x')
+  let countO = countPlayers(diag, 'o')
+  if((countX == 2 && countO == 0) || (countO == 2  && countX == 0)) {
+      let moveCoor = checkEmpty(diag, 'x');
+      if(moveCoor) {
+        return {x:moveCoor, y:Math.abs(moveCoor-2)};
+      }
+  }
+  return false;
+}
+
+function getDiagonal2 (moves, pos, data, player) {
+  if(pos == -1) {
+    return data;
+  } else {
+    let diag = moves.filter((move) => {
+        return (move.x == pos && move.y == Math.abs(pos-2));
+    })
+    if(diag[0]) {
+      data.push(diag[0])
+    }
+  }
+  return getDiagonal2(moves, pos-1, data, player);
+}
+
+function checkEmpty (moves, prop) {
+  let possiblePlaces = [0,1,2];
+  moves.forEach((move)=>{
+    let ind = possiblePlaces.indexOf(move[prop]);
+    if(ind > -1) {
+      possiblePlaces.splice(ind,1)
+    }
+  })
+  if(possiblePlaces.length == 1) {
+    return possiblePlaces[0];
+  } else {
+    return false;
+  }
+}
+
+function buildMove(x, y, player) {
+  return '[\"x\",' + x + ', \"y\", ' + y + ', \"v\", \"' + player + '\"]';
+}
+
+function countPlayers(arr, player) {
+  return arr.reduce((acc, move) => {
+    if(move.player == player) {
+      return acc + 1;
+    } else {
+     return acc;
+   }
+  },0);
+}
+
+function postRequest(url, body) {
   request({
-      url: url.slice(0, -1) + '2',
+      url: url,
       method: "POST",
-      body: "[[\"x\", 2, \"y\",  0, \"v\", \"o\"]]",
+      body: body,
       headers: {
        "Content-Type": "application/json+list"
      }
   }, function (error, response, body){
-      // console.log(response);
-      // console.log(response.statusCode);
   });
 }
 
-function getRequest(url) {
+function getRequest(url, player) {
   request({
     url: url,
     method: "GET",
     headers: { "Accept": "application/json+list" }
    }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        play(url, response.body);
+          let body = makeMove(response.body, player);
+          postRequest(url, body);
+          play(url, response.body, player);
       } else {
         console.log('Error during GET. Status code:', response.statusCode);
       }
@@ -52,7 +260,7 @@ function getRequest(url) {
 function parse(data) {
   let list = data.replace(/\s/g,'').substring(1);
   let moves = parseList(list, []);
-  console.log('moves',moves);
+  return moves;
 }
 
 function parseList(data, moves) {
@@ -72,7 +280,7 @@ function parseMove(data) {
   let coordY = parseInt(yPrefix[0],10);
   let playerPrefix = yPrefix.substring(7);
   let player = playerPrefix[0];
-  return [coordX, coordY, player];
+  return {x:coordX, y:coordY, player};
 }
 
 function trimToNextMove(data) {
